8d6a33ab21e72b499db5ffd173942b800ac7e3df
Aqudi <gjdigj@naver.com>
2019-04-01 20:05:02 +0900

    first commit

---
+++ taejung_jango/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\gjdigj145\desktop\mutsa\week1_django\taejung_jango\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
